e1ee8ded77badc97bbd9963b584ffadcccee9d4f
Stefan Geyer <stefangeyer@outlook.com>
2020-12-13 21:03:45 +0100

    Adjusts primary colors

--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -1,5 +1,5 @@
 const lightTheme = {
-    primary: 'rgba(215,113,88,1)',
+    primary: 'rgba(156,49,214,1)',
     text: 'rgba(58,52,51,1)',
     textSecondary: 'rgba(58,52,51,0.7)',
     background: 'rgba(255,255,255,1)',
@@ -9,7 +9,7 @@ const lightTheme = {
 }
 
 const darkTheme: Theme = {
-    primary: 'rgba(220,120,95,1)',
+    primary: 'rgba(214,30,148,1)',
     text: 'rgba(241,233,231,1)',
     textSecondary: 'rgba(241,233,231,0.6)',
     background: 'rgba(0,0,0,1)',
